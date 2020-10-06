4d2e97079c0c8d6a132ce1d91d01d707c6bddc14
Jaskaran Singh <jasksingh@egencia.com>
2020-10-07 00:53:17 +0530

    Removed unused import

--- src/main/java/com/jtech/service/football/configurations/BeanConfiguration.java
+++ src/main/java/com/jtech/service/football/configurations/BeanConfiguration.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.client.RestOperations;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
